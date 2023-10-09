92984b118353f773fbadf61514904ffb0be8bf38
ulugbekpy <ulugbekpycpp@gmail.com>
2023-10-09 13:19:43 +0500

    context processor for carts app

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, render
+from carts.models import CartItem
+from carts.views import _cart_id
 
 from category.models import Category
 from .models import Product
@@ -24,10 +26,15 @@ def product_detail(request, category_slug, product_slug):
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug
         )
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product
+        ).exists()
+
     except Exception as e:
-        print(f"{e}")
+        raise e
 
     contex = {
         "single_product": single_product,  # type: ignore
+        "in_cart": in_cart,  # type: ignore
     }
     return render(request, "store/product_detail.html", context=contex)
